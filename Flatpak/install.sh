748d8f8ee1c06f8e74087393a4d8ab9007d5c97a
hilltop804 <41207087+hilltop804@users.noreply.github.com>
2023-03-30 23:36:10 -0400

    Create install.sh

---
+++ Flatpak/install.sh
@@ -0,0 +1,7 @@
+cd GBA && flatpak install org.ExPlusAlpha.GBAemu.flatpak
+cd GBC && flatpak install org.ExPlusAlpha.GBCemu.flatpak
+cd MD && flatpak install org.ExPlusAlpha.MDemu.flatpak
+cd NEO && flatpak install org.ExPlusAlpha.NEOemu.flatpak
+cd NES && flatpak install org.ExPlusAlpha.NESemu.flatpak
+cd NGP && flatpak install org.ExPlusAlpha.NGPemu.flatpak
+cd Snes9x && flatpak install org.ExPlusAlpha.Snes9x.flatpak
