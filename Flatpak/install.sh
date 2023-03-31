a5149ddb8c9800a395da6e638734a8fd6ee8de10
hilltop804 <41207087+hilltop804@users.noreply.github.com>
2023-03-31 14:47:42 -0400

    Update install.sh

--- Flatpak/install.sh
+++ Flatpak/install.sh
@@ -1,7 +1,15 @@
-cd GBA && flatpak install org.ExPlusAlpha.GBAemu.flatpak
-cd GBC && flatpak install org.ExPlusAlpha.GBCemu.flatpak
-cd MD && flatpak install org.ExPlusAlpha.MDemu.flatpak
-cd NEO && flatpak install org.ExPlusAlpha.NEOemu.flatpak
-cd NES && flatpak install org.ExPlusAlpha.NESemu.flatpak
-cd NGP && flatpak install org.ExPlusAlpha.NGPemu.flatpak
-cd Snes9x && flatpak install org.ExPlusAlpha.Snes9x.flatpak
+flatpak --if-not-exists remote-add GBA GBA
+flatpak --if-not-exists remote-add GBC GBC
+flatpak --if-not-exists remote-add MD MD
+flatpak --if-not-exists remote-add NEO NEO
+flatpak --if-not-exists remote-add NES NES
+flatpak --if-not-exists remote-add NGP NGP
+flatpak --if-not-exists remote-add Snes9x Snes9x
+
+flatpak install --or-update GBA org.ExPlusAlpha.GBAemu
+flatpak install --or-update GBC org.ExPlusAlpha.GBCemu
+flatpak install --or-update MD org.ExPlusAlpha.MDemu
+flatpak install --or-update NEO org.ExPlusAlpha.NEOemu
+flatpak install --or-update NES org.ExPlusAlpha.NESemu
+flatpak install --or-update NGP org.ExPlusAlpha.NGPemu
+flatpak install --or-update Snes9x org.ExPlusAlpha.Snes9x
