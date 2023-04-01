8896d98ccb2082f8a741e406959743712900358e
hilltop804 <41207087+hilltop804@users.noreply.github.com>
2023-03-31 21:42:10 -0400

    Update install.sh

--- Flatpak/Bundle/install.sh
+++ Flatpak/Bundle/install.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 #Install/update emulator packages
 
-flatpak --if-not-exists --no-gpg-verify --user remote-add EMU ./Emu
+flatpak --if-not-exists --no-gpg-verify --user remote-add GBA ./Emu/GBA
+flatpak --if-not-exists --no-gpg-verify --user remote-add GBC ./Emu/GBC
+flatpak --if-not-exists --no-gpg-verify --user remote-add MD ./Emu/MD
+flatpak --if-not-exists --no-gpg-verify --user remote-add NEO ./Emu/NEO
+flatpak --if-not-exists --no-gpg-verify --user remote-add NES ./Emu/NES
+flatpak --if-not-exists --no-gpg-verify --user remote-add NGP ./Emu/NGP
+flatpak --if-not-exists --no-gpg-verify --user remote-add Snes9x ./Emu/Snes9x
 
-flatpak install -y --or-update EMU org.ExPlusAlpha.GBAemu
-flatpak install -y --or-update EMU org.ExPlusAlpha.GBCemu
-flatpak install -y --or-update EMU org.ExPlusAlpha.MDemu
-flatpak install -y --or-update EMU org.ExPlusAlpha.NEOemu
-flatpak install -y --or-update EMU org.ExPlusAlpha.NESemu
-flatpak install -y --or-update EMU org.ExPlusAlpha.NGPemu
-flatpak install -y --or-update EMU org.ExPlusAlpha.Snes9x
+flatpak install --or-update GBA org.ExPlusAlpha.GBAemu
+flatpak install --or-update GBC org.ExPlusAlpha.GBCemu
+flatpak install --or-update MD org.ExPlusAlpha.MDemu
+flatpak install --or-update NEO org.ExPlusAlpha.NEOemu
+flatpak install --or-update NES org.ExPlusAlpha.NESemu
+flatpak install --or-update NGP org.ExPlusAlpha.NGPemu
+flatpak install --or-update Snes9x org.ExPlusAlpha.Snes9x
 
 if [ ! -d /home/deck/.local/share/icons/exemu ]; then mkdir /home/deck/.local/share/icons/exemu; fi
 cp ./Icons/* /home/deck/.local/share/icons/exemu
